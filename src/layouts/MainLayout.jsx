8572e802f8e078b8f3c1caf6e8a76bac9e50cce3
WJKnight-tech <sirwilliam1206@gmail.com>
2025-01-01 22:36:57 +0000

    Install react router dom. Router working

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../components"
+import { Outlet } from "react-router-dom"
+
+
+const MainLayout = () => {
+ return (
+  <>
+   <Navbar />
+   <Outlet />
+  </>)
+}
+export default MainLayout
